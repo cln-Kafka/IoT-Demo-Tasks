b4e8233854efc4226541bcfa6bf4d87d5a05dd7c
Kareem Noureddine <100665578+cln-Kafka@users.noreply.github.com>
2024-11-24 16:23:11 +0200

    Blynk_CCTV_Demo - Not Finished

---
+++ Task05-Blynk_CCTV_Demo/esp_codes_gpt/esp_1_codes_gpt/esp_1_codes_gpt.ino
@@ -0,0 +1,84 @@
+#include <WiFi.h>
+#include <BlynkSimpleEsp32.h>
+#include <ESP32WebServer.h>
+#include <WiFiClient.h>
+#include <esp_camera.h>
+
+char auth[] = "YOUR_BLYNK_AUTH_TOKEN";  // Blynk Auth Token
+char ssid[] = "YOUR_WIFI_SSID";         // WiFi SSID
+char pass[] = "YOUR_WIFI_PASSWORD";     // WiFi Password
+
+ESP32WebServer server(80);
+
+void startCameraServer() {
+  server.on("/", HTTP_GET, [](){
+    server.send(200, "text/html", "<html><body><h1>ESP32 Camera Stream</h1><img src=\"/stream\"></body></html>");
+  });
+
+  server.on("/stream", HTTP_GET, [](){
+    WiFiClient client = server.client();
+    camera_fb_t * fb = esp_camera_fb_get();
+    if (!fb) {
+      server.send(500, "text/plain", "Failed to capture image");
+      return;
+    }
+
+    String header = "HTTP/1.1 200 OK\r\n"
+                    "Content-Type: image/jpeg\r\n"
+                    "Content-Length: " + String(fb->len) + "\r\n"
+                    "Connection: close\r\n\r\n";
+    server.sendContent(header);
+    server.sendContent((const char *)fb->buf, fb->len);
+    esp_camera_fb_return(fb);
+  });
+
+  server.begin();
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, pass);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+  Serial.println("Connected to WiFi");
+
+  Blynk.begin(auth, ssid, pass);
+
+  // Initialize the camera
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = 5;
+  config.pin_d1 = 18;
+  config.pin_d2 = 19;
+  config.pin_d3 = 21;
+  config.pin_d4 = 36;
+  config.pin_d5 = 39;
+  config.pin_d6 = 34;
+  config.pin_d7 = 35;
+  config.pin_xclk = 0;
+  config.pin_pclk = 22;
+  config.pin_vsync = 25;
+  config.pin_href = 23;
+  config.pin_sscb_sda = 26;
+  config.pin_sscb_scl = 27;
+  config.pin_pwdn = 32;
+  config.pin_reset = -1;
+  config.xclk_freq_hz = 20000000;
+  config.frame_size = FRAMESIZE_VGA;
+  config.jpeg_quality = 12;
+  config.fb_count = 2;
+
+  esp_camera_init(&config);
+
+  startCameraServer();
+}
+
+void loop() {
+  Blynk.run();
+  server.handleClient();
+}
